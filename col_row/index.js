50366eeeee0b1618c3e54dfb3456ac00755524d8
Denes Milan Kende <denes.milan@BOLYAI.LOCAL>
2026-02-06 13:06:11 +0100

    orai

---
+++ col_row/index.js
@@ -0,0 +1,35 @@
+/**
+ * @import {FormFieldType, HeaderArrayType, ColspanType, RowspanType} from './functions.js'
+ */
+
+import {Manager} from "./manager.js"
+import {Table} from "./table.js"
+import data from "./data.json" with {type: "json"}
+
+const manager = new Manager()
+manager.addCallback = (param) => {console.log(param)}
+
+for (const i of data.colspanDataArr)
+    manager.addElement(i)
+
+const table = new Table(data.colspanHeaderArray, manager)
+
+table.setAppendRow  ((tbody, row) => {
+    const tr = tbody.appendChild(document.createElement("tr"))
+
+    const td1 = tr.appendChild(document.createElement("td"))
+    td1.innerText = row.neve
+
+    const td2 = tr.appendChild(document.createElement("td"))
+    td1.innerText = row.kor
+
+    const td3 = tr.appendChild(document.createElement("td"))
+    td1.innerText = row.szerelme1
+
+    if (row.szerelme2){
+        const td4 = tr.appendChild(document.createElement("td"))
+        td4.innerText = row.szerelme2
+    } else {
+        td3.colSpan = 2
+    }
+})
